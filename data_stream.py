a8a8ec0bed24735b6dbc4481566e8a41322fb347
Stephen Lightcap <stephenlightcap121212@gmail.com>
2020-03-26 04:53:46 -0600

    first commit

---
+++ data_stream.py
@@ -0,0 +1,95 @@
+import logging
+import json
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+import pyspark.sql.functions as psf
+
+
+# TODO Create a schema for incoming resources
+schema = StructType([
+    StructField("offense_date", StringType(), True),
+    StructField("address_type", StringType(), True),
+    StructField("disposition", StringType(), True),
+    StructField("agency_id", StringType(), True),
+    StructField("common_location", StringType(), True),
+    StructField("city", StringType(), True),
+    StructField("state", StringType(), True),
+    StructField("call_date", StringType(), True),
+    StructField("call_date_time", StringType(), True),
+    StructField("report_date", StringType(), True),
+    StructField("crime_id", StringType(), True),
+    StructField("call_time", StringType(), True),
+    StructField("address", StringType(), True),
+    StructField("original_crime_type_name", StringType(), True)
+])
+
+def run_spark_job(spark):
+
+    df = spark \
+        .readStream \
+        .format('kafka') \
+        .option('kafka.bootstrap.servers', 'localhost:9092') \
+        .option('subscribe', 'com.udacity.police.service') \
+        .option('startingOffsets', 'earliest') \
+        .option('maxOffsetsPerTrigger', 10) \
+        .option('maxRatePerPartition', 10) \
+        .option('stopGracefullyOnShutdown', "true") \
+        .load()
+
+    df.printSchema()
+
+    kafka_df = df.selectExpr("CAST(value AS STRING)")
+
+    service_table = kafka_df\
+        .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
+        .select("DF.*")
+
+    distinct_table = service_table \
+        .select('original_crime_type_name', 'disposition', 'call_date_time') \
+        .distinct() \
+#         .withWatermark('call_date_time', "1 minute")
+
+    agg_df = distinct_table \
+        .dropna() \
+        .select('original_crime_type_name') \
+        .groupby('original_crime_type_name') \
+        .agg({'original_crime_type_name': 'count'}) \
+        .orderBy('count(original_crime_type_name)', ascending=True)
+
+    query = agg_df \
+        .writeStream \
+        .format('console') \
+        .outputMode('Complete') \
+        .start()
+
+
+    query.awaitTermination()
+
+    radio_code_json_filepath = "radio_code.json"
+    radio_code_df = spark.read.json(radio_code_json_filepath)
+
+    radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
+
+    join_query = agg_df \
+        .join(radio_code_df, col('agg_df.disposition') == col('radio_code_df.disposition'), 'left_outer')
+
+
+    join_query.awaitTermination()
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+
+    # TODO Create Spark in Standalone mode
+    spark = SparkSession \
+        .builder \
+        .master("local[*]") \
+        .appName("KafkaSparkStructuredStreaming") \
+        .config("spark.ui.port", 3000) \
+        .getOrCreate()
+
+    logger.info("Spark started")
+
+    run_spark_job(spark)
+
+    spark.stop()
---
+++ readme.md
@@ -0,0 +1,32 @@
+# SF Crime Statistics with Spark Streaming
+
+## Intro
+
+In this project, you will be provided with a real-world dataset, extracted from Kaggle, on San Francisco crime incidents, and you will provide statistical analyses of the data using Apache Spark Structured Streaming. You will draw on the skills and knowledge you've learned in this course to create a Kafka server to produce data, and ingest data through Spark Structured Streaming.
+
+## Development Environment
+
+You may choose to create your project in the workspace we provide here, or if you wish to develop your project locally, you will need to set up your environment properly as described below:
+
+- Spark 2.4.3
+- Scala 2.11.x
+- Java 1.8.x
+- Kafka build with Scala 2.11.x
+- Python 3.6.x or 3.7.x
+
+## Output Pictures
+
+
+## Questions
+
+### 1. How did changing values on the SparkSession property parameters affect the throughput and latency of the data?
+
+You can view the values `numInputRows`, `inputRowsPerSecond`, and `processedRowsPerSecond`. Changing the values of the below key / value pairs
+showed a difference in how much data was being processed.
+
+### 2. What were the 2-3 most efficient SparkSession property key/value pairs? Through testing multiple variations on values, how can you tell these were the most optimal?
+
+ - `maxRatePerPartition`
+ - `spark.default.parallelism`
+
+You can tell there was an optimal gain / loss by going off the above values in the first question.
